89cc56cc87c48114370574da8f7f05dec8160f21
Alejandro González Reyes <alexglzrys@gmail.com>
2020-09-20 21:46:33 -0500

    Publicar directorio build

---
+++ precache-manifest.e8fd0e06db2c7a4efbf66047a5a8a9e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a56af050a7be996fea45ed99c7afe2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a8e2fe6f6d6a1c282ea",
+    "url": "/static/css/2.8ec0c4ed.chunk.css"
+  },
+  {
+    "revision": "44fbe0a3d5d95a54024e",
+    "url": "/static/css/main.a7dd555d.chunk.css"
+  },
+  {
+    "revision": "1a8e2fe6f6d6a1c282ea",
+    "url": "/static/js/2.fcb888fa.chunk.js"
+  },
+  {
+    "revision": "55d06f978d8e1f7df225b8720c38fce5",
+    "url": "/static/js/2.fcb888fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fbe0a3d5d95a54024e",
+    "url": "/static/js/main.029ae69a.chunk.js"
+  },
+  {
+    "revision": "d962ce2b10124612a704",
+    "url": "/static/js/runtime-main.52bb842f.js"
+  }
+]);
