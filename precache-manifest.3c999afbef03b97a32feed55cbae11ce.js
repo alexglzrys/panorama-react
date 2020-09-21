1928992f17ef14f4618edf653f99ae09d18041b1
Alejandro González Reyes <alexglzrys@gmail.com>
2020-09-20 22:11:05 -0500

    Updates

---
+++ precache-manifest.3c999afbef03b97a32feed55cbae11ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0054a52c6383af1286f799ef8c27e4",
+    "url": "/panorama-react/index.html"
+  },
+  {
+    "revision": "1a8e2fe6f6d6a1c282ea",
+    "url": "/panorama-react/static/css/2.8ec0c4ed.chunk.css"
+  },
+  {
+    "revision": "44fbe0a3d5d95a54024e",
+    "url": "/panorama-react/static/css/main.a7dd555d.chunk.css"
+  },
+  {
+    "revision": "1a8e2fe6f6d6a1c282ea",
+    "url": "/panorama-react/static/js/2.fcb888fa.chunk.js"
+  },
+  {
+    "revision": "55d06f978d8e1f7df225b8720c38fce5",
+    "url": "/panorama-react/static/js/2.fcb888fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44fbe0a3d5d95a54024e",
+    "url": "/panorama-react/static/js/main.029ae69a.chunk.js"
+  },
+  {
+    "revision": "99fa1d869a84ceb5fe0d",
+    "url": "/panorama-react/static/js/runtime-main.b49f8c8f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8fd0e06db2c7a4efbf66047a5a8a9e9.js"
+  "/panorama-react/precache-manifest.3c999afbef03b97a32feed55cbae11ce.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/panorama-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
